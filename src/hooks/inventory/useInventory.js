6930b0918360732be777931c919feadf2b7a03e8
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-01-05 08:36:48 -0600

    add useInventory functions and inventorySlice actions

--- src/hooks/inventory/useInventory.js
+++ src/hooks/inventory/useInventory.js
@@ -1,11 +1,69 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { addItem, removeItem, updateItem as updateAction, toggleInPack as toggleAction } from "../../redux/inventorySlice";
+
+import { extractId } from "../../utils";
 
 export default function useInventory() {
   const inventorySlice = useSelector(state => state.inventory);
+  const dispatch = useDispatch()
 
   if (!inventorySlice) {
     throw new Error("useInventory must be used within a Redux Provider that contains an inventory reducer");
   };
 
-  return inventorySlice.inventory;
-}
+  const inventory = inventorySlice.inventory.map(item => (
+    {
+      ...item,
+      toggleInPack() {
+        dispatch(toggleAction({ id: item.id }));
+      },
+      openEdit() {
+
+      }
+
+    }
+  ));
+
+  const itemsInPack = inventory.filter(item => item.inPack);
+  const waterContainersInPack = inventory.filter(item => item.inPack && item.liquidCapacity > 0);
+
+  function getItemById(id) {
+    return inventory.find(item => item.id === id);
+  };
+
+  function addToInventory(newItem) {
+    dispatch(addItem({ item: newItem }));
+  };
+
+  function deleteFromInventory(itemOrId) {
+    const id = extractId(itemOrId);
+    dispatch(removeItem(removeItem({ id })));
+  };
+
+  function updateItem(oldItemOrId, newItem) {
+    const id = extractId(oldItemOrId);
+    dispatch(updateAction({ id, newItem }));
+  };
+
+  function filterByCategory(category, arrayOfItems) {
+    const items = arrayOfItems || inventory;
+
+    if (typeof items !== 'array') {
+      throw new Error("Invalid argument. filterByCategory requires array of items to filter")
+    };
+
+    return items.filter(item => item.category === category);
+  };
+
+  return {
+    inventorySlice,
+    inventory,
+    itemsInPack,
+    waterContainersInPack,
+    getItemById,
+    addToInventory,
+    deleteFromInventory,
+    updateItem,
+    filterByCategory
+  };
+};
--- src/redux/inventorySlice.js
+++ src/redux/inventorySlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import uuid from "react-native-uuid";
 
 import { inventory } from "../../archive/dummyData";
 
@@ -9,9 +10,66 @@ const inventorySlice = createSlice(
       inventory
     },
     reducers: {
+      addItem: (state, action) => {
+        const { item } = action.payload;
 
+        if (!item) {
+          throw new Error("No item recieved");
+        };
+
+        item.id = uuid.v4();
+        state.inventory.push(item);
+      },
+      removeItem: (state, action) => {
+        const { id } = action.payload;
+
+        if (!id || typeof id !== 'string') {
+          throw new Error("No item id recieved");
+        };
+
+        state.inventory = state.inventory.filter(item => item.id !== id);
+      },
+      updateItem: (state, action) => {
+        const { id, newItem } = action.payload;
+
+        if (!id || typeof id !== 'string') {
+          throw new Error("No item id recieved");
+        };
+
+        if (!newItem) {
+          throw new Error("newItem was not recieved");
+        };
+
+        const indexToUpdate = state.inventory.findIndex(item => item.id === id);
+
+        if (indexToUpdate === -1) {
+          throw new Error("Item with that ID was not found");
+        };
+
+        // ensure the original item id exists on the updated item object
+        newItem.id = state.inventory[indexToUpdate].id;
+
+        state.inventory[indexToUpdate] = newItem;
+      },
+      toggleInPack: (state, action) => {
+        const { id } = action.payload;
+
+        if (!id || typeof id !== 'string') {
+          throw new Error("No item id recieved");
+        };
+
+        const indexToUpdate = state.inventory.findIndex(item => item.id === id);
+
+        if (indexToUpdate === -1) {
+          throw new Error("Item with that ID was not found");
+        };
+
+        state.inventory[indexToUpdate].inPack = !state.inventory[indexToUpdate].inPack;
+      }
     }
   }
 );
 
 export default inventorySlice.reducer;
+
+export const { addItem, removeItem, updateItem, toggleInPack } = inventorySlice.actions;
