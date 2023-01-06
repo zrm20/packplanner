d5c698dac842f1798a0eae8a58c23ab5bc889af9
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-01-06 09:42:55 -0600

    convert weight and liquid conversions to ts

--- src/utils/liquidConversions/liquidConversions.ts
+++ src/utils/liquidConversions/liquidConversions.ts
@@ -1,11 +1,11 @@
 
-export function flOzToML(flOz) {
+export function flOzToML(flOz: number): number {
   //converts fl Oz to mL
   const exactMl = flOz * 29.5735;
   return Math.round(exactMl * 100) / 100;
 };
 
-export function mLToFlOz(mL) {
+export function mLToFlOz(mL: number): number {
   //converts mL to fl oz
   const exactOz = mL * 0.033814;
   return Math.round(exactOz * 100) / 100;
--- src/utils/weightConversions/weightConversions.ts
+++ src/utils/weightConversions/weightConversions.ts
@@ -1,34 +1,34 @@
-export function lbsToKg(lbs) {
+export function lbsToKg(lbs: number): number {
   //converts lbs to kg
   const exactKg = lbs * 0.453592;
   return Math.round(exactKg * 100) / 100;
 }
 
-export function ozToKg(oz) {
+export function ozToKg(oz: number): number {
   //converts oz to kg
   const exactKg = oz * 0.0283495;
   return Math.round(exactKg * 100) / 100;
 }
 
-export function ozToLbs(oz) {
+export function ozToLbs(oz: number): number {
   //converts oz to lbs
   return oz / 16;
 }
 
-export function kgToLbs(kg) {
+export function kgToLbs(kg: number): number {
   //converts kg to lbs
   const exactLbs = kg * 2.20462;
   const roundedLbs = Math.round(exactLbs * 100) / 100;
   return roundedLbs;
 }
 
-export function kgToOz(kg) {
+export function kgToOz(kg: number): number {
   //converts kgToOz
   const exactOz = kg * 35.274;
   return Math.round(exactOz);
 }
 
-export function kgToLbsOz(kg) {
+export function kgToLbsOz(kg: number): number[] {
   //returns array of [0]lbs, and [1]remaining oz;
   let oz = kgToOz(kg);
   let lbs = Math.floor(oz / 16);
@@ -36,6 +36,6 @@ export function kgToLbsOz(kg) {
   return [lbs, oz];
 }
 
-export function roundedKg(kg) {
+export function roundedKg(kg: number): number {
   return Math.round(kg * 100) / 100;
 };
