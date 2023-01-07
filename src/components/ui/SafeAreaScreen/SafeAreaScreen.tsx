4b13af1ac97e93665f3cbde15bbe3bc0e3138364
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-01-07 15:00:51 -0600

    convert SafeAreaScreen to ts

---
+++ src/components/ui/SafeAreaScreen/SafeAreaScreen.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, StatusBar, ViewStyle } from "react-native";
+import { useTheme } from "react-native-paper";
+import { SafeAreaViewProps } from "react-native-safe-area-context";
+
+import { isAndroid } from "../../../utils";
+
+interface Props extends SafeAreaViewProps {
+  alignCenter?: boolean;
+  justifyCenter?: boolean;
+};
+
+interface Style {
+  container: ViewStyle
+};
+
+export default function SafeAreaScreen(props: Props): JSX.Element {
+  const { alignCenter = false, justifyCenter = false, ...rest } = props;
+  const theme = useTheme();
+
+  const styles = StyleSheet.create<Style>(
+    {
+      container: {
+        marginTop: isAndroid() ? StatusBar.currentHeight : 0,
+        flex: 1,
+        alignItems: alignCenter ? 'center' : null,
+        justifyContent: justifyCenter ? 'center' : null,
+        backgroundColor: theme.colors.background
+      }
+    }
+  );
+
+  return (
+    <SafeAreaView {...rest} style={[styles.container, props.style]}>
+      {props.children}
+    </SafeAreaView>
+  )
+};
