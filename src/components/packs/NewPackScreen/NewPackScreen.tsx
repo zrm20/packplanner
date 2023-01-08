1386f211b5909769cd5fdbf0a20882deafbce5fe
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-01-08 08:14:39 -0600

    convert NewPackScreen to ts

--- src/components/packs/NewPackScreen/NewPackScreen.tsx
+++ src/components/packs/NewPackScreen/NewPackScreen.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { TouchableWithoutFeedback, View, Keyboard } from "react-native";
 import { FAB, Title } from "react-native-paper";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 
+import type { LockerStackParamList } from "../../../navigation/navigation.types";
 import { isAndroid } from "../../../utils";
 import { SafeAreaScreen } from "../../ui";
 import { usePacks } from "../../../hooks";
 import PackForm from "../PackForm/PackForm";
-import useStyles from "./NewPackScreen.styles"
+import useStyles from "./NewPackScreen.styles";
 
-export default function NewPackScreen({ navigation, ...props }) {
+type NewPackScreenProps = NativeStackScreenProps<LockerStackParamList, 'NewPack'>;
+
+export default function NewPackScreen({ navigation }: NewPackScreenProps): JSX.Element {
   const styles = useStyles();
   const { addPack } = usePacks();
 
-  function handleSubmit(pack) {
+  function handleSubmit(pack: PackData): void {
     addPack(pack);
     navigation.goBack();
   };
