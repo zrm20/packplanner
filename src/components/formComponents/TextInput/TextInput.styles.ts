5d036e2030efe8ed25fc969f84e93d09d040fff5
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-01-08 10:49:30 -0600

    convert TextInput to ts

---
+++ src/components/formComponents/TextInput/TextInput.styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet, ViewStyle } from "react-native";
+
+import { useTheme } from "../../../theme";
+
+interface Style {
+  container: ViewStyle;
+}
+
+export default function useStyles() {
+  const theme = useTheme();
+
+  const styles = StyleSheet.create<Style>(
+    {
+      container: {
+        width: '100%',
+        marginBottom: theme.space[2],
+        paddingHorizontal: theme.space[3]
+      }
+    }
+  );
+  return styles;
+};
--- src/components/formComponents/TextInput/TextInput.tsx
+++ src/components/formComponents/TextInput/TextInput.tsx
@@ -1,28 +1,37 @@
 import React from "react";
 import { View } from "react-native";
-import { HelperText, TextInput as PaperInput } from "react-native-paper";
+import { HelperText, TextInput as PaperInput, TextInputProps, withTheme } from "react-native-paper";
 import { useField } from "formik";
 
 import useStyles from "./TextInput.styles"
 
-export default function TextInput(props) {
+type CustomTextInputProps = TextInputProps  & {
+  name: string
+  label?: string,
+};
+
+function TextInput(props: CustomTextInputProps): JSX.Element {
   const styles = useStyles();
-  const [field, meta, util] = useField(props)
+  const [field, meta, util] = useField(props.name);
+
+  const showError: boolean = Boolean(meta.touched && meta.error);
 
   return (
     <View style={[styles.container, props.style]} >
       <PaperInput
         value={field.value}
         onChangeText={util.setValue}
-        error={meta.touched && meta.error}
+        error={showError}
         label={props.label || props.name}
         mode="outlined"
         {...props}
       />
       {
-        meta.touched && meta.error &&
+        showError &&
         <HelperText type="error">{meta.error}</HelperText>
       }
     </View>
   );
 };
+
+export default withTheme(TextInput);
