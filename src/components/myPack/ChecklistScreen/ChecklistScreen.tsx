8b06cb08d7ac1be9f49e58e489e98efebef277a4
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-02-12 09:37:08 -0600

    add isPacked count

--- src/components/myPack/ChecklistScreen/ChecklistScreen.tsx
+++ src/components/myPack/ChecklistScreen/ChecklistScreen.tsx
@@ -5,18 +5,24 @@ import { Checkbox, Divider, List, Text } from "react-native-paper";
 import useStyles from "./ChecklistScreen.styles";
 import { SafeAreaScreen } from "../../ui";
 import { useInventory } from "../../../hooks";
+import { useTheme } from "../../../theme";
 
 export default function ChecklistScreen(): JSX.Element {
   const styles = useStyles();
+  const { colors } = useTheme()
   const { itemsInPack } = useInventory();
 
   itemsInPack.sort((a, b) => {
     return a.isPacked ? 1 : -1 // sort unpacked items to end
   });
 
+  const totalItems = itemsInPack.length;
+  const packedItems = itemsInPack.filter(item => item.isPacked).length
+
   return (
     <SafeAreaScreen style={styles.container} >
       <Text variant="headlineMedium" style={styles.title}>Trip Checklist</Text>
+      <Text variant="labelLarge" style={styles.title}>{`${packedItems} of ${totalItems} packed`}</Text>
 
       <View style={styles.listContainer}>
         <FlatList
@@ -31,6 +37,7 @@ export default function ChecklistScreen(): JSX.Element {
                   <Checkbox.Android
                     status={item.isPacked ? "checked" : "unchecked"}
                     onPress={item.toggleIsPacked}
+                    color={colors.tertiary}
                   />
                 )}
               />
