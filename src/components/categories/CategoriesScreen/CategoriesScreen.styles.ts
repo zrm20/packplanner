9480c061260be26ff79351ba94c7cbdac53c9096
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-02-18 17:09:43 -0600

    add build the CategoriesScreen;

--- src/components/categories/CategoriesScreen/CategoriesScreen.styles.ts
+++ src/components/categories/CategoriesScreen/CategoriesScreen.styles.ts
@@ -4,6 +4,10 @@ import { useTheme } from "../../../theme";
 
 interface Style {
   container: ViewStyle;
+  titleContainer: ViewStyle;
+  listItem: ViewStyle;
+  listContainer: ViewStyle;
+  surface: ViewStyle;
 };
 
 export default function useStyles() {
@@ -12,7 +16,31 @@ export default function useStyles() {
   const styles = StyleSheet.create<Style>(
     {
       container: {
-
+        width: '100%',
+        padding: theme.space[2],
+        flex: 1,
+      },
+      titleContainer: {
+        flexDirection: 'row',
+        borderBottomWidth: theme.borderWeight[1],
+        borderColor: theme.colors.secondary,
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        paddingHorizontal: theme.space[2],
+      },
+      listContainer: {
+        flex: 1,
+        padding: theme.space[2],
+      },
+      listItem: {
+        width: '100%',
+        borderColor: theme.colors.outlineVariant,
+        borderBottomWidth: theme.borderWeight[1]
+      },
+      surface: {
+        padding: theme.space[2],
+        width: '100%',
+        height: '100%'
       }
     }
   );
--- src/components/categories/CategoriesScreen/CategoriesScreen.tsx
+++ src/components/categories/CategoriesScreen/CategoriesScreen.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { FlatList, View } from "react-native";
+import { Text, IconButton, Surface, List } from "react-native-paper";
+import { useCategories } from "../../../hooks";
 
 import { SafeAreaScreen } from "../../ui";
 
@@ -12,10 +13,41 @@ interface CategoriesScreenProps {
 
 export default function CategoriesScreen(props: CategoriesScreenProps): JSX.Element {
   const styles = useStyles();
+  const { categories } = useCategories();
 
   return (
-    <SafeAreaScreen style={styles.container} >
-      <Text>Categories</Text>
+    <SafeAreaScreen>
+      <View style={styles.container}>
+        <View style={styles.titleContainer}>
+          <Text variant="titleLarge">Categories</Text>
+          <IconButton icon="plus" size={14} mode="outlined" />
+        </View>
+
+        <View style={styles.listContainer}>
+          <Surface style={styles.surface}>
+            <FlatList
+              data={categories}
+              keyExtractor={item => item.id}
+              renderItem={({ item }) => (
+                <List.Item
+                  style={styles.listItem}
+                  key={item.id}
+                  title={item.label}
+                  left={props => <List.Icon icon={item.icon} />}
+                  right={props => (
+                    <View>
+                      {
+                        item.isStockCategory &&
+                        <Text variant="labelSmall">Stock</Text>
+                      }
+                    </View>
+                  )}
+                />
+              )}
+            />
+          </Surface>
+        </View>
+      </View>
     </SafeAreaScreen>
   );
 };
