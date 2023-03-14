8fca727130164e1c2538ed0c72a05018ac072b67
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-03-14 18:05:54 -0500

    update to handle guest logout

--- src/components/settings/UserWidget/UserWidget.tsx
+++ src/components/settings/UserWidget/UserWidget.tsx
@@ -1,7 +1,6 @@
-import { useNavigation } from "@react-navigation/native";
-import { getAuth } from "firebase/auth";
 import React from "react";
-import { View, ViewStyle } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { View, ViewStyle, Alert } from "react-native";
 import { Surface, Text, Button } from "react-native-paper";
 import useUser from "../../../hooks/user/useUser";
 import { LoadingBackdrop } from "../../ui";
@@ -17,6 +16,24 @@ export default function UserWidget(props: UserWidgetProps): JSX.Element {
   const { navigate } = useNavigation();
   const { user, logout, isLoading } = useUser();
 
+  function handleGuestLogout(): void {
+    Alert.alert(
+      "Warning, you will lose your data!",
+      "You are about to log out as a guest. All of your data including inventory, packs and lists will be lost.\n\nYou can register for an account to save your data online. Do you want to continue logging out?",
+      [
+        {
+          text: "Yes, erase my data and log out",
+          style: "destructive",
+          onPress: logout
+        },
+        {
+          text: "Cancel",
+          style: "cancel"
+        }
+      ]
+    )
+  };
+
   return (
     <>
       <LoadingBackdrop show={isLoading} />
@@ -31,7 +48,15 @@ export default function UserWidget(props: UserWidgetProps): JSX.Element {
           </Text>
 
           <View style={styles.buttonGroup}>
-            <Button mode="contained" onPress={logout}>Logout</Button>
+            {
+              "email" in user! ?
+                <Button mode="contained" onPress={logout}>Logout</Button> :
+                <>
+                  <Button mode="contained">Register</Button>
+                  <Button mode="contained">Login</Button>
+                  <Button mode="contained" onPress={handleGuestLogout}>Logout</Button>
+                </>
+            }
           </View>
         </Surface>
       </View>
