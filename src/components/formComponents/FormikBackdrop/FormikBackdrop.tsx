5f7ae6b5cdc26f55dca79d9d8a4c0152dbb27a43
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-03-25 11:47:13 -0500

    wrap in portal to render at top level

--- src/components/formComponents/FormikBackdrop/FormikBackdrop.tsx
+++ src/components/formComponents/FormikBackdrop/FormikBackdrop.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import { useFormikContext } from "formik";
 
 import { LoadingBackdrop } from "../../ui";
+import { Portal } from "react-native-paper";
 
 export default function FormikBackdrop(): JSX.Element {
   const { isSubmitting } = useFormikContext();
 
-  return <LoadingBackdrop show={isSubmitting} />;
+  return (
+    <Portal>
+      <LoadingBackdrop show={isSubmitting} />
+    </Portal>
+  )
 };
